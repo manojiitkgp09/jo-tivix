a6906f59a18389170f3f29f17fa07dcbdb0638ad
Manoj Agarwal <manoj.agarwal@reef.pl>
2019-11-14 11:33:53 +0530

    Add user model test cases

--- app/src/tsm/core/tests.py
+++ app/src/tsm/core/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username="teacher1", password="teacher1")
+        User.objects.create(username="teacher2", password="teacher2", first_name="first", last_name="last")
+
+    def test_user_has_name_as_username(self):
+        """Animals that can speak are correctly identified"""
+        user = User.objects.get(username="teacher1")
+        self.assertEqual(user.name, 'teacher1')
+
+    def test_user_has_name_as_first_name_last_name(self):
+        """Animals that can speak are correctly identified"""
+        user = User.objects.get(username="teacher2")
+        self.assertEqual(user.name, 'first last')
+
+    def test_string_representation(self):
+        user = User.objects.get(username="teacher2")
+        self.assertEqual(str(user), 'teacher2')
